be3607045d4b8799d97b74f6614a027ee7e52b43
tiago <tiagobg15@gmail.com>
2020-07-30 19:19:31 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mongoose iot base
+
+This project contains a base for iot devices using mongoose os
+
+## Tested Hardware
+
+- ESP8266
---
+++ mjs_fs/api_iot.js
@@ -0,0 +1,32 @@
+load('api_config.js')
+load('api_mqtt.js')
+
+let IOT = {
+  template: {
+    LIGHT: 'light',
+    LIGHT_RGB: 'light_rgb',
+    LIGHT_BRIGHTNESS: 'light_brightness',
+    SMARTLOCK: 'smartlock',
+  },
+  isPairingMode: function() {
+    return Cfg.get('wifi.ap.enable')
+  },
+  setPairingMode: function() {
+    print('Set pairing mode')
+    Cfg.set({ wifi: { ap: { enable: !Cfg.get('wifi.ap.enable') } } })
+  },
+  register: function(template = this.template.LIGHT, properties) {
+    let mqttTopic = Cfg.get('mqtt_events')
+    let device = {
+      event: 'register_device',
+      device_id: Cfg.get('device.id'),
+      device_name: Cfg.get('custom.device_name'),
+      user_email: Cfg.get('custom.user_email'),
+      properties: properties,
+      device_template: template,
+      topic_events: mqttTopic,
+    }
+    print('Register device', JSON.stringify(device))
+    MQTT.pub(mqttTopic, JSON.stringify(device), 0)
+  },
+}
